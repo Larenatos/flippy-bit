75c35616506aee55fb14db2b89de78a23bfa17a5
Lare1426 <laurikj.virta@gmail.com>
2022-09-24 21:45:40 +0300

    added a binary box

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 import pygame
 pygame.init()
 
-pygame.display.set_mode(size=(500, 400))
-surface = pygame.display.get_surface()
+screen = pygame.display.set_mode(size=(500, 400))
+screen.fill("grey")
 
 rect_colour = "blue"
-pygame.draw.rect(surface, "yellow", pygame.Rect(30, 30, 60, 60), 5)
-rectangle = pygame.draw.rect(surface, rect_colour, pygame.Rect(35, 35, 50, 50))
+rectangle = pygame.draw.rect(screen, rect_colour, pygame.Rect(35, 35, 50, 50))
+binary_box_background = pygame.draw.rect(screen, "#06001a", pygame.Rect(150, 150, 70, 70))
+
+binary_rect = pygame.Rect(0, 0, 80, 80)
+binary_rect.center = binary_box_background.center
+binary_box_border = pygame.draw.rect(screen, "#8c8c8c", binary_rect, 5)
+
+font = pygame.font.SysFont(None, 70)
+binary_box_text = font.render("0", True, "white")
+binary_box_text_rect = binary_box_text.get_rect()
+binary_box_text_rect.center = binary_box_background.center
+screen.blit(binary_box_text, binary_box_text_rect)
 pygame.display.flip()
 
 while True:
@@ -20,5 +30,5 @@ while True:
             rect_colour = "red"
           else:
             rect_colour = "blue"
-          pygame.draw.rect(surface, rect_colour, pygame.Rect(35, 35, 50, 50))
+          pygame.draw.rect(screen, rect_colour, pygame.Rect(35, 35, 50, 50))
           pygame.display.update()
