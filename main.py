b0580f674017ce130c5a41abf58f3c60f666de21
Lare1426 <laurikj.virta@gmail.com>
2022-09-24 19:48:59 +0300

    Added functionality to change colour of a rectangle with a click of a mousebutton

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ pygame.init()
 pygame.display.set_mode(size=(500, 400))
 surface = pygame.display.get_surface()
 
-
+rect_colour = "blue"
 pygame.draw.rect(surface, "yellow", pygame.Rect(30, 30, 60, 60), 5)
-pygame.draw.rect(surface, "blue", pygame.Rect(35, 35, 50, 50))
+rectangle = pygame.draw.rect(surface, rect_colour, pygame.Rect(35, 35, 50, 50))
 pygame.display.flip()
 
 while True:
   for event in pygame.event.get():
-    if event.type == pygame.QUIT: pygame.quit()
+    match event.type:
+      case pygame.QUIT:
+        pygame.quit()
+      case pygame.MOUSEBUTTONDOWN:
+        if rectangle.collidepoint(pygame.mouse.get_pos()):
+          if rect_colour == "blue":
+            rect_colour = "red"
+          else:
+            rect_colour = "blue"
+          pygame.draw.rect(surface, rect_colour, pygame.Rect(35, 35, 50, 50))
+          pygame.display.update()
