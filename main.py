0df68287cca922657e007ddade6243e7cdfe1592
Lare1426 <laurikj.virta@gmail.com>
2022-09-25 14:00:13 +0300

    removed extra space

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ class BinaryBox():
     self.position = position
     self.dimensions = (50, 50)
     self.current_binary = "0"
-
     self.border_rect = pygame.Rect((0, 0), self.dimensions)
     self.background_rect = pygame.Rect (self.position, (self.dimensions[0]- 10, self.dimensions[1] - 10))
     self.border_rect.center = self.background_rect.center
