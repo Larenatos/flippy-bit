ed4085827c6be9e8e1dea2b1de4656c81971b2af
Lare1426 <laurikj.virta@gmail.com>
2022-11-12 16:32:44 +0200

    removed match case to check if the key is space to just if statement

--- main.py
+++ main.py
@@ -66,19 +66,18 @@ while True:
           pygame.quit()
           exit()
         case pygame.KEYDOWN:
-          match event.key:
-            case pygame.K_SPACE:
-              game.state_of_game = True
-              draw_layout(game, binary_boxes, binary_bar_preview, score_display)
-              # resetting values to default
-              time_between_spawns = 5
-              mergers = {}
-              shot_missiles = {}
-              game.alive_enemies = []
-              score_display.text_content = "0"
-              for box in binary_boxes:
-                if box.current_bit: 
-                  box.flip_bit()
+          if event.key == pygame.K_SPACE:
+            game.state_of_game = True
+            draw_layout(game, binary_boxes, binary_bar_preview, score_display)
+            # resetting values to default
+            time_between_spawns = 5
+            mergers = {}
+            shot_missiles = {}
+            game.alive_enemies = []
+            score_display.text_content = "0"
+            for box in binary_boxes:
+              if box.current_bit: 
+                box.flip_bit()
     continue
 
   current_time = time()
